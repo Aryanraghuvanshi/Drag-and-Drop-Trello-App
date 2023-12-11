21e2d47c5981c589fdfc311b8900b4281d4eab8a
Aryan Raghuvanshi <aryanraghuvanshi91@gmail.com>
2023-12-11 20:01:40 +0530

    Added Drag and Drop functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
@@ -20,7 +20,8 @@ let onHoldListArray = [];
 let listArrays = []
 
 // Drag Functionality
-
+let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -59,6 +60,8 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
   listEl.textContent = item;
+  listEl.draggable =true;
+  listEl.setAttribute('ondragstart','drag(event)');
   //Append
 
   columnEl.appendChild(listEl);
@@ -96,5 +99,33 @@ function updateDOM() {
 
 }
 
+//When item start dragging
+function drag(e){
+  draggedItem = e.target;
+  console.log(draggedItem);
+}
+
+// Column Allows For item to drop
+function allowDrop(e){
+  e.preventDefault();
+} 
+
+// when the item enter column area
+function dragEnter(column){
+  listColumns[column].classList.add('over')
+  currentColumn =column;
+}
+
+// Dropping Item in column
+function drop(e){
+  e.preventDefault();
+  // remove backgrond color padding
+  listColumns.forEach((column)=>{
+    column.classList.remove('over')
+  })
+  //add item to column
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
+}
 // On load
 updateDOM();
